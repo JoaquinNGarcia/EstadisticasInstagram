17556fde2da0a9183a91b70f2b61c70379ce2c5f
Gabriel Corvalán <gabrielcor134@gmail.com>
2023-04-25 20:16:01 -0300

    Menú

--- src/com/estadisticasInstagram/Menu.java
+++ src/com/estadisticasInstagram/Menu.java
@@ -2,6 +2,8 @@ package com.estadisticasInstagram;
 
 import com.estadisticasInstagram.controladora.PerfilInstagram;
 import java.util.*;
+import java.lang.String;
+
 
 public class Menu {
     private PerfilInstagram perfilInstagram;
@@ -31,5 +33,12 @@ public class Menu {
                 "   ##     ##   ##  ##   ##    ##     ##  ##    ##  ##   ##  ##  ##  ##   ##   ##\n" +
                 "  ####    ##   ##   #####    ####    ##  ##     #####  #### ##  ##  ##   ##   ##\n" +
                 "\n\n");
+        System.out.println("\033[0;1m" + "\n========================================================================" );
+        System.out.println("\033[0;1m" + "============================" +  " MENÚ PRINCIPAL " +  "\033[0;1m" + "============================" );
+        System.out.println("\033[0;1m" + "========================================================================\n" );
+        System.out.println("\033[0;1m" +  "1" +  " - ABM de las publicaciones y albumes del perfil.\n" +
+                "\033[0;1m" + "2" + " - Consultar publicaciones.\n" +
+                "\033[0;1m" + "3" +  " - Generar estadisticas.\n" +
+                "\033[0;1m" + "4" +  " - Generar reportes.\n");
     }
    }
